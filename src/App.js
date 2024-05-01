819151363e87a7ebeef5a1bed0a8d9c3a169854e
Alsu Ismayilzade <131715293+alsu-ismayilzada@users.noreply.github.com>
2024-05-01 12:47:16 +0400

    update

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ export default function App() {
   return <div><Wine
      title = "Sparkling wine Le Grand Noir Brut Reserve Rose"
      description = "France,rose,0.75l."
-     rating = "3.6"
+     rating = {3.6}
+     like = {true}
        />
        <Wine 
      title = "Wine"
      description = "Italy,rose,0.5l."
-     rating = "3.6"
+     rating = {3.6}
+     like = {false}
        />
        </div>
 }
--- src/Wine.js
+++ src/Wine.js
@@ -1,11 +1,14 @@
 import "./style.css";
+import heart from "./heart.png"
 
 export default function Wine(props) {
+  let isLike=true;
     return (
       <div className="common">
        <p className="title">{props.title}</p>
        <p className="description">{props.description}</p>
        <p className="rating">{props.rating}</p>
+       {isLike == props.like ? <img src={heart} /> : null}
       </div>
     );
   }
