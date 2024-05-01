5063f5d64bf321d6ec27959b27fc204f25bedcd8
Alsu Ismayilzade <131715293+alsu-ismayilzada@users.noreply.github.com>
2024-05-01 12:25:07 +0400

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Wine from "./Wine";
 
 export default function App() {
-  return <div><Wine 
+  return <div><Win 
      title = "Sparkling wine Le Grand Noir Brut Reserve Rose"
      description = "France,rose,0.75l."
      rating = "3.6"
